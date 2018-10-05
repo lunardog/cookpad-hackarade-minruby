dae345ab13606af44152fc9e20268831fd6142aa
Leszek <leszek-rybicki@cookpad.com>
2018-10-05 15:40:30 +0900

    script to test stuff

---
+++ alltester.rb
@@ -0,0 +1,28 @@
+require 'minruby'
+require 'colorize'
+require 'pp'
+require 'stringio'
+
+MY_PROGRAM = ARGV.join(" ")
+
+
+puts "Testing #{MY_PROGRAM}"
+
+Dir.glob('test*.rb').sort.each do |f|
+  correct = `ruby #{f}`
+  answer = `ruby #{MY_PROGRAM} #{f}`
+
+  print "#{f} => "
+  if correct == answer
+    puts "OK!".green
+  else
+    puts "NG".red
+
+    out = StringIO.new
+    PP.pp(minruby_parse(File.read(f)), out)
+    out.rewind
+    puts out.read.yellow
+
+    exit(1)
+  end
+end
