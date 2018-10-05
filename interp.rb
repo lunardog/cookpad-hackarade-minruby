e0c7a0c5d9a01fc1dd36dfd058de4b71de0187f0
Leszek <leszek-rybicki@cookpad.com>
2018-10-05 10:50:10 +0900

    fizzbuzz, thx to @sto

--- interp.rb
+++ interp.rb
@@ -113,7 +113,23 @@ def evaluate(exp, env)
       when "Integer"
         evaluate(exp[2], env).to_i
       when "fizzbuzz"
-        pp exp[2]
+        n = 1
+        while n < 100
+          if n % 3 == 0
+            if n % 5 == 0
+              p("FizzBuzz")
+            else
+              p("Fizz")
+            end
+          else
+            if n % 5 == 0
+              p("Buzz")
+            else
+              p(n)
+            end
+          end
+          n = n + 1
+        end
       else
         raise("unknown builtin function")
       end
