857318ecdccb1ac14dc43fbbbc0cbb9264f0360e
Leszek <leszek-rybicki@cookpad.com>
2018-10-05 10:41:31 +0900

    if and boolean operators

--- interp.rb
+++ interp.rb
@@ -27,6 +27,14 @@ def evaluate(exp, env)
     evaluate(exp[1], env) / evaluate(exp[2], env)
   when "%"
     evaluate(exp[1], env) % evaluate(exp[2], env)
+  when "=="
+    evaluate(exp[1], env) == evaluate(exp[2], env)
+  when ">"
+    evaluate(exp[1], env) > evaluate(exp[2], env)
+  when "<"
+    evaluate(exp[1], env) < evaluate(exp[2], env)
+  when "!="
+    evaluate(exp[1], env) != evaluate(exp[2], env)
   # ... Implement other operators that you need
 
 
@@ -72,11 +80,17 @@ def evaluate(exp, env)
     #   else
     #     ???
     #   end
-    raise(NotImplementedError) # Problem 3
+    if evaluate(exp[1], env)
+      evaluate(exp[2], env)
+    else
+      evaluate(exp[3], env)
+    end
 
   when "while"
     # Loop.
-    raise(NotImplementedError) # Problem 3
+    while evaluate(exp[1], env)
+      evaluate(exp[2], env)
+    end
 
 
 #
